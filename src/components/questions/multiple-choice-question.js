1a2a4614c6fdec6eea47ad3a080a6a6b1a29faf2
unknown <weiweiliu2018@outlook.com>
2021-04-17 22:49:35 -0400

    Finished true-false-question UI

--- src/components/questions/multiple-choice-question.js
+++ src/components/questions/multiple-choice-question.js
@@ -1,22 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 
 const MultipleChoiceQuestion = ({question}) => {
+    const [yourAnswer, setYourAnswer] = useState("")
+
+    const grade = (yourAnswer) => {
+        if (yourAnswer === question.correct) {
+
+        } else {
+
+        }
+    }
+
     return(
         <div>
-            <h4>{question.question}</h4>
-            {question.correct}
-            {
-                question.choices.map((choice) => {
-                    return(
-                        <label>
-                            <input type="radio" name={question._id}/>
-                            {choice}
-                        </label>
+            <h5>
+                {question.question}
+                {
+                    question.correct === yourAnswer &&
+                    <i className="fas fa-check"></i>
+                }
+                {
+                    question.correct !== yourAnswer &&
+                    <i className="fas fa-times"></i>
+                }
+            </h5>
+
+            <ul className="list-group w-50">
+                {
+                    question.choices.map((choice) =>
+                            <li className={`list-group-item 
+                            ${choice === question.correct && choice === yourAnswer ? 'list-group-item-success' : ''}`}>
+                                <label><input
+                                    onClick={() => {
+                                        setYourAnswer(choice)
+                                    }}
+                                    type="radio"
+                                    name={question._id}/> {choice}</label>
+                            </li>
                     )
-                })
-            }
+                }
+            </ul>
+
+            <p>Your answer: {yourAnswer}</p>
+            <p>{question.correct}</p>
+            <button type="button" className="btn btn-success"
+                    onClick={grade}>
+                Grade
+            </button>
         </div>
     )
 }
 
-export default MultipleChoiceQuestion;
+export default MultipleChoiceQuestion
--- src/components/questions/true-false-question.js
+++ src/components/questions/true-false-question.js
@@ -2,32 +2,79 @@ import React, {useState} from "react";
 
 const TrueFalseQuestion = ({question}) => {
     const [answer, setAnswer] = useState(null)
+    const [trueClass, setTrueClass] = useState('')
+    const [falseClass, setFalseClass] = useState('')
+
+    const answerString = JSON.stringify(answer)
+
+    const grade = () => {
+
+        if (answerString == question.correct) {
+            if (question.correct == 'true') {
+                setTrueClass('list-group-item-success')
+                setFalseClass('')
+            } else {
+                setFalseClass('list-group-item-success')
+                setTrueClass('')
+            }
+        } else {
+            if (question.correct == 'true') {
+                setTrueClass('list-group-item-success')
+                setFalseClass('list-group-item-danger')
+            } else {
+                setFalseClass('list-group-item-success')
+                setTrueClass('list-group-item-danger')
+            }
+            // {
+            //     question.correct === 'true' &&
+            //     setTrueClass('list-group-item-success') &&
+            //     setFalseClass('list-group-item-danger')
+            // }
+            // {
+            //     question.correct === 'false' &&
+            //     setFalseClass('list-group-item-success') &&
+            //     setTrueClass('list-group-item-danger')
+            // }
+        }
+    }
+
     return (
         <div>
             <h4>
                 {question.question}
                 {
-                    answer == question.correct &&
+                    answerString == question.correct &&
                     <i className="fas fa-check"></i>
                 }
                 {
-                    answer != question.correct &&
+                    answerString != question.correct &&
                     <i className="fas fa-times"></i>
                 }
             </h4>
-            {question.correct}
-            <br/>
-            {JSON.stringify(answer)}
-            <br/>
-            <label><input
-                type="radio"
-                onClick={() => setAnswer(true)}
-                name={question._id}/>True</label>
-            <br/>
-            <label><input
-                type="radio"
-                onClick={() => setAnswer(false)}
-                name={question._id}/>False</label>
+
+            <ul className="list-group w-50">
+                <li className={`list-group-item ${trueClass}`}>
+                    <label><input
+                        type="radio"
+                        onClick={() => setAnswer(true)}
+                        name={question._id}/>True</label>
+                </li>
+                <li className={`list-group-item ${falseClass}`}>
+                    <label><input
+                        type="radio"
+                        onClick={() => setAnswer(false)}
+                        name={question._id}/>False</label>
+                </li>
+            </ul>
+
+            <p>
+                Your answer: {answerString}
+            </p>
+
+            <button type="button" className="btn btn-success"
+                    onClick={grade}>
+                Grade
+            </button>
         </div>
     )
 }
