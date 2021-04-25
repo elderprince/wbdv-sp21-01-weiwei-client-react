1a2f4361b631106db4701f485ebcdd973cee8d43
unknown <weiweiliu2018@outlook.com>
2021-04-25 13:46:55 -0400

    Added quiz-attempt service

--- src/services/quiz-service.js
+++ src/services/quiz-service.js
@@ -10,8 +10,21 @@ const findQuizById = (qid) => {
         .then(response => response.json() )
 }
 
+const submitQuiz = (quizId, questions) => {
+    fetch(`{QUIZ_URL}/quizzes/${quizId}/attempts`, {
+        method: 'POST',
+        body: JSON.stringify(questions),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(response => response.json())
+        .then(result => console.log(result))
+}
+
 const api = {
-    findAllQuizzes, findQuizById
+    findAllQuizzes,
+    findQuizById,
+    submitQuiz
 }
 
 export default api;
